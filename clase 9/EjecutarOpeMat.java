9444f82432eecdcf78981480f1d20ede9d482e36
Luis-ucc <luis.escobara@campusucc.edu.co>
2025-10-01 20:47:27 +0000

    uso del metodo

---
+++ clase 9/EjecutarOpeMat.java
@@ -0,0 +1,11 @@
+public class EjecutarOpeMat {
+
+    public static void main(String[] args) {
+        
+        OperacionesMatematicas objOp = new OperacionesMatematicas(8,6);
+
+
+        System.out.println("El resultado de la suma es: " + objOp.sumar());
+    }
+    
+}
---
+++ clase 9/OperacionesMatematicas.java
@@ -0,0 +1,19 @@
+public class OperacionesMatematicas {
+//atributos
+    private int n1;
+    private int n2;
+
+    //constructor de la clase
+
+    public OperacionesMatematicas(int n1, int n2){
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+    
+    //metodos
+    public int sumar(){
+        return n1 + n2;
+    }
+
+    
+}
