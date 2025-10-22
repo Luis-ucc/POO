c34bc9650813255e061f05fdb7d05e5012ae3498
Luis-ucc <luis.escobara@campusucc.edu.co>
2025-10-22 21:35:49 +0000

    update

--- clase 12/ejemplo 2/Vehiculo.java
+++ clase 12/ejemplo 2/Vehiculo.java
@@ -1,12 +1,17 @@
 public class Vehiculo {
-  protected String marca ;
-   protected int velocidad ;
- public Vehiculo ( String marca ) {
-this . marca = marca ;
-this . velocidad = 0;
-}
-public void acelerar () { velocidad += 10; }
-public String info () {
-return " V e h i c u l o " + marca + " va a " + velocidad + " km / h " ;
-}
+  protected String marca;
+  protected int velocidad;
+
+  public Vehiculo ( String marca ) {
+    this.marca = marca;
+    this.velocidad = 0;
+  }
+
+  public void acelerar () { 
+    velocidad += 10; 
+  }
+
+  public String info () {
+   return "Vehiculo " + marca + " va a " + velocidad + " km/h";
+   }
 }
