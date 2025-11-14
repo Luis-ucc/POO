6fbe6b4dafa584fdaa3a228ccc39ca882acf3c78
Luis-ucc <luis.escobara@campusucc.edu.co>
2025-11-14 03:14:33 +0000

    organizacion

--- ascensor/Puerta.java
+++ ascensor/Puerta.java
@@ -2,19 +2,16 @@ class Puerta {
 
     private boolean abierta = false; 
 
-
     public void abrir() { 
         abierta = true; 
         System.out.println(" Puerta abierta."); 
     } 
 
- 
     public void cerrar() { 
         abierta = false; 
         System.out.println(" Puerta cerrada."); 
     } 
 
- 
     public boolean isAbierta() { 
         return abierta; 
     } 
